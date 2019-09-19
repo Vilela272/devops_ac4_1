9eeaf9eff4c2e92633320c867b3f14385fcdf4fd
Guilherme Vilela Moreira <1900486@SALAS.AULAS>
2019-09-19 20:59:08 -0300

    Alterando a operação

--- com/vilela/operacoes.py
+++ com/vilela/operacoes.py
@@ -1,11 +1,10 @@
 class Elevador:
 
-    andar_atual = 0  
-    quantidade_pessoas = 0
-
     def __init__(self, capacidade, total_andares):
+        self.andar_atual = 0
         self.capacidade = capacidade
         self.total_andares = total_andares
+        self.quantidade_pessoas = 0
 
     def entrar(self):
         if self.quantidade_pessoas < self.capacidade:
@@ -16,15 +15,15 @@ class Elevador:
             self.quantidade_pessoas -= 1
 
     def subir(self):
-        if self.andar_atual < self.total_andares:
+        if self.total_andares > self.andar_atual:
             self.andar_atual += 1
-
+            
     def descer(self):
         if self.andar_atual > 0:
-            self.andar_atual -= 1 
-
+            self.andar_atual -= 1
+            
     def ir_para_andar(self, andar):
-        if andar > self.total_andares or andar < 0:
+        if andar > self.total_andares or andar < 0:          # Correcao do professor
             self.andar_atual = self.andar_atual
         else:
             self.andar_atual = andar
